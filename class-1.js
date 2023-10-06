358766ffdf5e2b5374b742220f743622ff12027d
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-10-06 18:25:10 +0100

    adds class one file

---
+++ class-1.js
@@ -0,0 +1,15 @@
+console.log("Hello from JavaScript");
+
+// variable declarations
+let name = "jack";
+var age = 29;
+const gender = "male";
+
+// string concatenation;
+// joining strings together
+
+// my name is jack, i am a 29 year old male
+console.log("my name is " + name + ", i am a " + age + " year old " + gender);
+
+const GRAVITY = 9.8;
+console.log(GRAVITY);
